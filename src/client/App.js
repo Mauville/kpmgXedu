ee7e80fc3abaf1289fab3ce33195445d027ceec0
karla-rg <58357385+karla-rg@users.noreply.github.com>
2021-06-03 03:01:33 -0500

    Fix Add.js

--- src/client/App.js
+++ src/client/App.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { ConnectedRouter } from 'connected-react-router';
-<<<<<<< HEAD
 import { Switch, Route  } from 'react-router-dom';
-=======
-import { Switch, Route } from 'react-router-dom';
->>>>>>> master
 import PrivateRoute from './components/PrivateRoute/PrivateRoute';
 import GuestRoute from './components/GuestRoute/GuestRoute';
 import CheckIfLoggedIn from './components/CheckIfLoggedIn/CheckIfLoggedIn';
@@ -13,10 +9,7 @@ import LoginPage from './pages/Login/Login';
 import RegisterPage from './pages/Register/Register';
 import DashboardPage from './pages/Dashboard/Dashboard';
 import PageNotFound from './pages/NotFound/NotFound';
-<<<<<<< HEAD
 import Busqueda from './components/Front/Busqueda';
-=======
->>>>>>> master
 
 const App = props => {
   return (
@@ -26,12 +19,8 @@ const App = props => {
           <PrivateRoute exact path="/" component={DashboardPage} />
           <GuestRoute exact path="/login" component={LoginPage} />
           <GuestRoute exact path="/register" component={RegisterPage} />
-<<<<<<< HEAD
           <Route path="/busqueda" component={Busqueda} />
           { <Route component={PageNotFound} /> }
-=======
-          <Route component={PageNotFound} />
->>>>>>> master
         </Switch>
       </CheckIfLoggedIn>
     </ConnectedRouter>
