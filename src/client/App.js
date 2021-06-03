1e6f5498c4988723ebc2d0d23438d1336df248e2
Mauville <4991127@outlook.com>
2021-06-03 01:53:12 -0500

    Repair client

--- src/client/App.js
+++ src/client/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { ConnectedRouter } from 'connected-react-router';
-import { Switch /*, Route */ } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import PrivateRoute from './components/PrivateRoute/PrivateRoute';
 import GuestRoute from './components/GuestRoute/GuestRoute';
 import CheckIfLoggedIn from './components/CheckIfLoggedIn/CheckIfLoggedIn';
@@ -9,7 +9,7 @@ import CheckIfLoggedIn from './components/CheckIfLoggedIn/CheckIfLoggedIn';
 import LoginPage from './pages/Login/Login';
 import RegisterPage from './pages/Register/Register';
 import DashboardPage from './pages/Dashboard/Dashboard';
-// import PageNotFound from './pages/NotFound/NotFound';
+import PageNotFound from './pages/NotFound/NotFound';
 
 const App = props => {
   return (
@@ -19,7 +19,7 @@ const App = props => {
           <PrivateRoute exact path="/" component={DashboardPage} />
           <GuestRoute exact path="/login" component={LoginPage} />
           <GuestRoute exact path="/register" component={RegisterPage} />
-          {/* <Route component={PageNotFound} /> */}
+          <Route component={PageNotFound} />
         </Switch>
       </CheckIfLoggedIn>
     </ConnectedRouter>
